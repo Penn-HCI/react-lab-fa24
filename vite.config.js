beb2dc80d14fbde15a7732d348f75ef1b251b0bf
michellechang02 <changmi@seas.upenn.edu>
2024-08-28 13:59:07 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-lab-fa24/', // Replace with your repository name
   plugins: [react()],
-})
+});
